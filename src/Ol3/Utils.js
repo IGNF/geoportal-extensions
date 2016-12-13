5dda1e3a60bc642ee52d353e27a9d82e4a3d5e58
lowzonenose <jpbazonnais@gmail.com>
2016-12-13 12:50:46 +0100

    fix. sur le profil par defaut de l'extension ElevationPath (ol et leaflet)

--- src/Ol3/Utils.js
+++ src/Ol3/Utils.js
@@ -124,7 +124,9 @@ define([
             var tileGrid = source.tileGrid;
             var tileCoord = source.tileGrid.getTileCoordForCoordAndResolution(coordinate, resolution);
 
-            /*this code is duplicated from createFromWMTSTemplate function*/
+            /*
+            * this code is duplicated from createFromWMTSTemplate function
+            */
             var getTransformedTileCoord = function (tileCoord, tileGrid, projection) {
                 var tmpTileCoord = [0,0,0]; /*Note : [z(zoomLevel),x,y]*/
                 var tmpExtent = ol.extent.createEmpty();
