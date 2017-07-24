25c108ec57133f9c52b3fba5779f31ed931efdd7
Elias Couppe <elias.couppe@gmail.com>
2017-07-24 10:12:46 +0200

    [IT] Attributions control - adaptations pour lecture originators

--- src/Itowns/Controls/Attribution.js
+++ src/Itowns/Controls/Attribution.js
@@ -1,11 +1,13 @@
 define([
     "Common/Utils",
     "Common/Utils/SelectorID",
+    "Common/Utils/LayerUtils",
     "Common/Controls/AttributionDOM",
     "Itowns/Controls/Widget"
 ], function (
     Utils,
     SelectorID,
+    LayerUtils,
     AttributionDOM,
     Widget
 ) {
@@ -223,14 +225,31 @@ define([
 
             if (ori) {
                 for (var j = 0; j < ori.length; j++) {
+                    // si l'attribution est déjà ajoutée, on passe pour ne pas l'ajouter plusieurs fois
                     if (attributions.has(ori[j].name)) {
                         continue;
                     };
-
+                    // s'il n'y a pas de contraintes associées à l'originator, on rajoute l'attribution et on continue
+                    if (!ori[j].constraints) {
+                        // on ajoute l'attribution dans la Map() 'attributions'
+                        attributions.set(ori[j].name, ori[j]);
+                        continue;
+                    }
                     // si l'attribut minScaleDenominator existe
                     if (ori[j].constraints[0].minScaleDenominator) {
-                        // on vérifie qu'on se situe bien entre minScaleDenominator et maxScaleDenominator
-                        if (!(ori[j].constraints[0].minScaleDenominator < scaleDenominator && scaleDenominator < ori[j].constraints[0].maxScaleDenominator)) {
+                        // si min/maxScaleDenominator sont égaux, on veut afficher l'attribution pour le niveau de zoom associé au scale dénominator
+                        if (ori[j].constraints[0].minScaleDenominator === ori[j].constraints[0].maxScaleDenominator) {
+                            // on récupère le niveau de zoom 
+                            var attributionZoomLevel = LayerUtils.getZoomLevelFromScaleDenominator(ori[j].constraints[0].minScaleDenominator);
+                            // on sélectionne une fourchette de scaledenominators autour du niveau de zoom qui correspond
+                            var maxAttributionScaleDenominator = (this._resolutionsWGS84[attributionZoomLevel] + this._resolutionsWGS84[attributionZoomLevel - 1]) / (0.00028 * 2);
+                            var minAttributionScaleDenominator = (this._resolutionsWGS84[attributionZoomLevel] + this._resolutionsWGS84[attributionZoomLevel + 1]) / (0.00028 * 2);
+                            if (!(maxAttributionScaleDenominator > scaleDenominator && scaleDenominator > minAttributionScaleDenominator)) {
+                                continue;
+                            }
+                        }
+                        // sinon, on vérifie qu'on se situe bien entre minScaleDenominator et maxScaleDenominator
+                        else if (!(ori[j].constraints[0].minScaleDenominator < scaleDenominator && scaleDenominator < ori[j].constraints[0].maxScaleDenominator)) {
                             continue;
                         }
                     }
@@ -277,6 +296,31 @@ define([
             ul.appendChild(li);
         });
         this._attributionListContainer.appendChild(ul);
+    };
+
+    Attribution.prototype._resolutionsWGS84 = {
+        0 : 156543.033928041,
+        1 : 78271.51696402048,
+        2 : 39135.758482010235,
+        3 : 19567.87924100512,
+        4 : 9783.93962050256,
+        5 : 4891.96981025128,
+        6 : 2445.98490512564,
+        7 : 1222.99245256282,
+        8 : 611.49622628141,
+        9 : 305.7481131407048,
+        10 : 152.8740565703525,
+        11 : 76.43702828517624,
+        12 : 38.21851414258813,
+        13 : 19.10925707129406,
+        14 : 9.554628535647032,
+        15 : 4.777314267823516,
+        16 : 2.388657133911758,
+        17 : 1.194328566955879,
+        18 : 0.5971642834779395,
+        19 : 0.2985821417389697,
+        20 : 0.1492910708694849,
+        21 : 0.0746455354347424
     };
 
     return Attribution;
