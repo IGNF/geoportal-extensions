f8f49195be58f5078b2c99bd91a6dfccc0da2963
lowzonenose <jpbazonnais@gmail.com>
2017-10-06 14:20:32 +0200

    mise en place des templates avec handlebars...

---
+++ samples-src-handlebars/gulpfile.js
@@ -0,0 +1,35 @@
+var gulp  = require("gulp");
+var clean = require("gulp-clean");
+var hb    = require("gulp-hb");
+
+gulp.task("build", [], function () {
+    var hbStream = hb({
+        cwd : process.cwd(),
+        debug : true
+    })
+    .partials('./templates/partials/*.hbs')
+    .partials('./templates/partials/leaflet/*.hbs')
+    .partials('./templates/leaflet/*.hbs')
+    .helpers(require('handlebars-layouts'))
+    .data({
+        baseurl : '../../..'
+    });
+
+    return gulp
+        .src('./pages/leaflet/**/*.html')
+        .pipe(hbStream)
+        .pipe(gulp.dest('./build'));
+});
+
+gulp.task("clean", [], function () {
+    var stream = gulp.src([
+        "build"
+    ], {
+        force : true
+    });
+    return stream.pipe(clean());
+});
+
+gulp.task("default", ["clean"], function () {
+  gulp.start("build");
+});
