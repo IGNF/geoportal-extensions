e15b3900a6f656a6e1263ff0ee5bd2dc60aad5f5
lowzonenose <jpbazonnais@gmail.com>
2017-10-18 10:13:54 +0200

    cf. issue #162, mise à jour 'scripts/release.sh'

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,30 +1,117 @@
 #!/bin/bash
 
-set -x
+# FIXME
+# authentification github pour 'git push'
+# https://help.github.com/articles/which-remote-url-should-i-use/#cloning-with-ssh-urls
 
-PWD=`pwd`
+# FIXME
+# authentification npm pour 'npm publish'
+# idem avec bower...
 
-# version
-VERSION=0.0.1
+# TODO
+# impl. les options longues
+# Ex. --leaflet
 
-# librairie
-LIBRARY="leaflet"
+# set -x
+
+# répertoire d'execution
+_PWD=`pwd`
+
+# version du shell
+_VERSION=0.0.1
+
+# librairie par defaut
+_LIBRARY="leaflet"
+
+# options
+#   -leaflet
+#   (-debug )
+#   -no-build |b, -no-data |d, -no-json |j, -no-info |i, -no-commit |o, -no-publish |p, -no-clean |c
+_OPTS_RUN_BUILD=false
+_OPTS_RUN_DATA=false
+_OPTS_RUN_JSON=false
+_OPTS_RUN_INFO=false
+_OPTS_RUN_COMMIT=false
+_OPTS_RUN_PUBLISH=false
+_OPTS_RUN_CLEAN=false
+
+# parse options
+while getopts "lhbdjicpC" opt; do
+  case ${opt} in
+    h|help )
+      echo "Usage :"
+      echo "    `basename $0` -h    Affiche cette aide."
+      echo "    l (--leaflet)     Execution de la publication Leaflet (par defaut),"
+      echo "    o (--ol3)         Execution de la publication Openlayers,"
+      echo "    b (--run-build)   Execution de la tache de compilation,"
+      echo "    d (--run-data)    Execution de la tache de git-clone,"
+      echo "    j (--run-json)    Execution de la tache de creation des json,"
+      echo "    i (--run-info)    Execution de la tache de creation du fichier d'info,"
+      echo "    c (--run-commit)  Execution de la tache de git-push,"
+      echo "    p (--run-publish) Execution de la tache de publication npm et bower,"
+      echo "    C (--run-clean)   Execution de la tache de nettoyage."
+      echo "Ex. `basename $0` -bdjicpC"
+      exit 0
+      ;;
+   \? )
+     echo "Invalid Option: -$OPTARG" 1>&2
+     exit 1
+     ;;
+   l|leaflet )
+      _LIBRARY="leaflet"
+      GIT_DIR_PUBLISH=${_GIT_DIR_PUBLISH_LEAFLET}
+      GIT_REPOSITORY=${_GIT_REPOSITORY_LEAFLET}
+     ;;
+   o|ol3 )
+      _LIBRARY="ol3"
+     ;;
+   b|build )
+      _OPTS_RUN_BUILD=true
+     ;;
+   d|data )
+      _OPTS_RUN_DATA=true
+     ;;
+   j|json )
+      _OPTS_RUN_JSON=true
+     ;;
+   i|info )
+      _OPTS_RUN_INFO=true
+     ;;
+   c|commit )
+     _OPTS_RUN_COMMIT=true
+     ;;
+   p|publish )
+      _OPTS_RUN_PUBLISH=true
+     ;;
+   C|clean )
+      _OPTS_RUN_CLEAN=true
+     ;;
+  esac
+  shift $((OPTIND -1))
+done
 
 # chemins des répertoires
-DIR_SCRIPTS="${PWD}/scripts"
-DIR_SRC="${PWD}/src"
-DIR_DIST="${PWD}/dist"
+_DIR_SCRIPTS="${_PWD}/scripts"
+_DIR_SRC="${_PWD}/src"
+_DIR_DIST="${_PWD}/dist"
 
 # chargement des properties
-PROPERTIES="${DIR_SCRIPTS}/release.ini"
-source ${PROPERTIES}
+_PROPERTIES="${_DIR_SCRIPTS}/release.ini"
+source ${_PROPERTIES}
 
 # git
-_GIT_COMMIT_MESSAGE=${GIT_COMMIT_MESSAGE}
-_GIT_FILES_ADD=${GIT_FILES_ADD}
-_GIT_DIR_PUBLISH=${GIT_DIR_PUBLISH}
-_GIT_REPOSITORY=${GIT_REPOSITORY}
-_GIT_REPOSITORY_TOKEN=${GIT_REPOSITORY_TOKEN}
+GIT_COMMIT_MESSAGE=${_GIT_COMMIT_MESSAGE}
+GIT_FILES_ADD=${_GIT_FILES_ADD}
+
+[ ${_LIBRARY} == "leaflet" ] && {
+  GIT_DIR_PUBLISH=${_GIT_DIR_PUBLISH_LEAFLET}
+  GIT_REPOSITORY=${_GIT_REPOSITORY_LEAFLET}
+}
+
+[ ${_LIBRARY} == "ol3" ] && {
+  GIT_DIR_PUBLISH=${_GIT_DIR_PUBLISH_OPENLAYERS}
+  GIT_REPOSITORY=${_GIT_REPOSITORY_OPENLAYERS}
+}
 
 ##########
 # doCmd()
@@ -55,13 +142,20 @@ info () {
     cat >&2 <<EOF
 
 ----------------------------------------------------------
-`basename $0`, version ${VERSION}
+`basename $0`, version ${_VERSION}
 ----------------------------------------------------------
---> Publication des releases ${LIBRARY} <--
--- Configuration : ${PROPERTIES}
--- Information   :
---      url (git)   : ${_GIT_REPOSITORY}
---      depot (git) : ${_GIT_DIR_PUBLISH}
+--> Publication des releases ${_LIBRARY} <--
+-- Configuration : ${_PROPERTIES}
+-- Parametres : ...
+--    build   : ${_OPTS_RUN_BUILD}
+--    data    : ${_OPTS_RUN_DATA}
+--    json    : ${_OPTS_RUN_JSON}
+--    info    : ${_OPTS_RUN_INFO}
+--    git     : ${_OPTS_RUN_COMMIT}
+--    publish : ${_OPTS_RUN_PUBLISH}
+--    clean   : ${_OPTS_RUN_CLEAN}
+-- Information   : ...
+--    depot Git : ${GIT_REPOSITORY}
 ----------------------------------------------------------
 
 EOF
@@ -77,79 +171,127 @@ printTo "BEGIN"
 ################################################################################
 printTo "--> build..."
 
-# # gulp --leaflet
-# doCmd "gulp --leaflet" > /dev/null 2>&1
-# # gulp publish --leaflet
-# doCmd "gulp publish --leaflet" > /dev/null 2>&1
-# # gulp --production --leaflet
-# doCmd "gulp --production --leaflet" > /dev/null 2>&1
-# # gulp publish --leaflet
-# doCmd "gulp publish --leaflet" > /dev/null 2>&1
+if [ ${_OPTS_RUN_BUILD} == true ]
+then
+  doCmd "cd ${_PWD}"
+  doCmd "gulp --${_LIBRARY}" > /dev/null 2>&1
+  doCmd "gulp publish --${_LIBRARY}" > /dev/null 2>&1
+  doCmd "gulp --production --${_LIBRARY}" > /dev/null 2>&1
+  doCmd "gulp publish --${_LIBRARY}" > /dev/null 2>&1
+fi
 
 ################################################################################
 printTo "--> data"
 
-# git clone https://github.com/lowzonenose/geoportal-extensions-leaflet.git
-doCmd "git clone https://github.com/${_GIT_REPOSITORY}.git ${_GIT_DIR_PUBLISH}"
-# cp -r dist/leaflet geoportal-extensions-leaflet/dist
-doCmd "cp -r ${DIR_DIST}/${LIBRARY}/. ${_GIT_DIR_PUBLISH}/dist"
-# cp LICENCE.md geoportal-extensions-leaflet/
-doCmd "cp LICENCE.md ${_GIT_DIR_PUBLISH}"
-# cp README-leaflet.md geoportal-extensions-leaflet/README.md
-doCmd "cp README-leaflet.md ${_GIT_DIR_PUBLISH}/README.md"
-# cp CHANGELOG_DRAFT.md geoportal-extensions-leaflet/CHANGELOG.md
-doCmd "cp CHANGELOG_DRAFT.md ${_GIT_DIR_PUBLISH}/CHANGELOG.md"
+if [ ${_OPTS_RUN_DATA} == true ]
+then
+  doCmd "cd ${_PWD}"
+  # git clone https://github.com/lowzonenose/geoportal-extensions-leaflet.git
+  doCmd "git clone ${GIT_REPOSITORY} ${GIT_DIR_PUBLISH}"
+  # cp -r dist/leaflet geoportal-extensions-leaflet/dist
+  doCmd "cp -r ${_DIR_DIST}/${_LIBRARY}/. ${GIT_DIR_PUBLISH}/dist"
+  # cp LICENCE.md geoportal-extensions-leaflet/
+  doCmd "cp LICENCE.md ${GIT_DIR_PUBLISH}"
+  # cp README-leaflet.md geoportal-extensions-leaflet/README.md
+  doCmd "cp README-leaflet.md ${GIT_DIR_PUBLISH}/README.md"
+  # cp CHANGELOG_DRAFT.md geoportal-extensions-leaflet/CHANGELOG.md
+  doCmd "cp CHANGELOG_DRAFT.md ${GIT_DIR_PUBLISH}/CHANGELOG.md"
+fi
 
 ################################################################################
 printTo "--> json"
 
-package_version=$(cat package.json | perl -MJSON -0ne 'my $DS = decode_json $_;print $DS->{version};')
-package_content=$(cat ${_GIT_DIR_PUBLISH}/package.json | perl -MJSON -0ne 'my $DS = decode_json $_; $DS->{version} = ${package_version}; print encode_json $DS')
-bower_content= $(cat ${_GIT_DIR_PUBLISH}/bower.json | perl -MJSON -0ne 'my $DS = decode_json $_; $DS->{version} = ${package_version}; print encode_json $DS')
-doCmd "echo ${package_content} > ${_GIT_DIR_PUBLISH}/package.json"
-doCmd "echo ${bower_content} > ${_GIT_DIR_PUBLISH}/bower.json"
+# version API
+_PACKAGE_VERSION=$(cat package.json | perl -MJSON -0ne 'my $DS = decode_json $_;print $DS->{version};')
+
+if [ ${_OPTS_RUN_JSON} == true ]
+then
+  [ -d ${GIT_DIR_PUBLISH} ] && {
+    # contenu du package.json avec version API
+    _PACKAGE_CONTENT=$(cat ${GIT_DIR_PUBLISH}/package.json |
+        perl -MJSON -0ne '
+          my $DS = decode_json $_;
+          $DS->{version} = ${_PACKAGE_VERSION};
+          print encode_json $DS
+          ')
+    # contenu du bower.json avec version API
+    _BOWER_CONTENT=$(cat ${GIT_DIR_PUBLISH}/bower.json |
+        perl -MJSON -0ne '
+          my $DS = decode_json $_;
+          $DS->{version} = ${_PACKAGE_VERSION};
+          print encode_json $DS
+          ')
+
+    doCmd "cd ${_PWD}"
+    doCmd "echo ${_PACKAGE_CONTENT} > ${GIT_DIR_PUBLISH}/package.json"
+    doCmd "echo ${_BOWER_CONTENT} > ${GIT_DIR_PUBLISH}/bower.json"
+  }
+fi
 
 ################################################################################
 printTo "--> info"
 
-# $(cat geoportal-extensions-leaflet/summary.json | perl -MJSON -0ne '
-#   my $DS = decode_json $_;
-#   $DS->{version} = $version;
-#   $DS->{date} = ...;
-#   $DS->{} = ...;
-#   $DS->{} = ...;
-#   print encode_json $DS
-# ')
+# TODO informations sur la construcion des binaires
+# Ex. date, version, size, md5, ...
+if [ ${_OPTS_RUN_INFO} == true ]
+then
+  [ -d ${GIT_DIR_PUBLISH} ] && {
+    _INFO_CONTENT=$(cat ${GIT_DIR_PUBLISH}/summary.json |
+    perl -MJSON -0ne '
+      my $DS = decode_json $_;
+      $DS->{version} = "...";
+      $DS->{date} = "...";
+      print encode_json $DS
+    ')
+    echo ${_INFO_CONTENT} > "${GIT_DIR_PUBLISH}/summary.json"
+  }
+fi
 
 ################################################################################
-printTo "--> tag"
+printTo "--> git"
 
-# git add -Af
-doCmd "git add -Af ${_GIT_DIR_PUBLISH}"
-# git commit -m "Publication Leaflet version 0.10.0"
-message=$(echo ${_GIT_COMMIT_MESSAGE} | sed -e "s@%version%@$package_version@g" | sed -e "s@%library%@$LIBRARY@g")
-doCmd "git commit -m \"$message\"  ${_GIT_DIR_PUBLISH}"
-# git push
-doCmd ""
+# FIXME git push : authentification ?
+if [ ${_OPTS_RUN_COMMIT} == true ]
+then
+  [ -d ${GIT_DIR_PUBLISH} ] && {
+    doCmd "cd ${GIT_DIR_PUBLISH}"
+    doCmd "git add -Af"
+    message=$(echo ${GIT_COMMIT_MESSAGE} |
+        sed -e "s@%version%@${_PACKAGE_VERSION}@g" |
+        sed -e "s@%library%@${_LIBRARY}@g")
+    doCmd "git commit -m \"$message\""
+    doCmd "git push"
+  }
+fi
 
 ###############################################################################
 printTo "--> publish"
 
-# npm publish
-doCmd ""
-# npm install bower
-doCmd ""
-# bower register geoportal-extensions-leaflet http://github.com/IGNF/geoportal-extensions-leaflet.git
-doCmd ""
+# FIXME npm / bower : authentification ?
+if [ ${_OPTS_RUN_PUBLISH} == true ]
+then
+  [ -d ${GIT_DIR_PUBLISH} ] && {
+    doCmd "cd ${GIT_DIR_PUBLISH}"
+    # npm publish
+    doCmd ""
+    # npm install bower
+    doCmd ""
+    # bower register geoportal-extensions-leaflet http://github.com/IGNF/geoportal-extensions-leaflet.git
+    doCmd ""
+  }
+fi
 
 ###############################################################################
 printTo "--> clean"
 
-# rm -rf geoportal-extensions-leaflet/
-doCmd ""
+if [ ${_OPTS_RUN_CLEAN} == true ]
+then
+  doCmd "cd ${_PWD}"
+  doCmd "rm -rf ${GIT_DIR_PUBLISH}"
+fi
 
 printTo "END"
 
-set +x
+# set +x
 
 exit 0
