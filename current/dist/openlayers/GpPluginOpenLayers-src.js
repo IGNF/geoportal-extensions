84cdbc4986ba6cc6926f85038ebc4514820a672a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-02 17:07:33 +0000

    Deploy IGNF/geoportal-extensions to github.com/IGNF/geoportal-extensions.git:gh-pages

--- current/dist/openlayers/GpPluginOpenLayers-src.js
+++ current/dist/openlayers/GpPluginOpenLayers-src.js
@@ -85416,8 +85416,7 @@ var MeasureArea = function (Control) {
     var measure;
 
     if (this.options.geodesic) {
-      var sourceProj = map.getView().getProjection();
-      var geom = polygon.clone().transform(sourceProj, "EPSG:4326");
+      var geom = polygon.clone();
       var coordinates = geom.getLinearRing(0).getCoordinates();
       measure = Math.abs(Object(ol_sphere__WEBPACK_IMPORTED_MODULE_2__["getArea"])(new ol_geom__WEBPACK_IMPORTED_MODULE_3__["Polygon"]([coordinates])));
     } else {
