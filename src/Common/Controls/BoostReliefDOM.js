16800ec49afaa75e99e239524f9670ad15503a8a
Elias Couppe <elias.couppe@gmail.com>
2020-04-23 17:17:31 +0200

    feat(it:Controls) : implémente un control d'exageration du relief
    Ce controle permet d'appliquer un coefficient multiplicateur aux valeurs d'elevation d'une couche MNT.
    L'utilisateur peut definir l'echelle et le pas sur lesquels il veut "jouer"
    
    Reste à faire :
    - CSS : bouton, setCollapsed
    - definition des parametres : plusieurs couches MNTs gerees independemment?
    - jsdoc
    - tests
    
    Voir RDEV-34387

---
+++ src/Common/Controls/BoostReliefDOM.js
@@ -0,0 +1,159 @@
+var BoostReliefDOM = {
+
+    // ################################################################### //
+    // ######################### Main container ########################## //
+    // ################################################################### //
+
+    /**
+    * Add uuid to the tag ID
+    * @param {String} id - id selector
+    * @returns {String} uid - id selector with an unique id
+    */
+    _addUID : function (id) {
+        var uid = (this._uid) ? id + "-" + this._uid : id;
+        return uid;
+    },
+
+    /**
+     * Creation du container principal (DOM)
+     *
+     * @returns {DOMElement} container - layer switcher DOM element
+     */
+    _createMainContainerElement : function () {
+        var container = document.createElement("div");
+        container.id = this._addUID("GPboostRelief");
+        container.className = "GPwidget";
+        return container;
+    },
+
+    // ################################################################### //
+    // ############################ Relief tool ########################### //
+    // ################################################################### //
+
+    /**
+     * Creation du container des outils avancés du layer (DOM)
+     *
+     * @param {Object} brOptions - options du control boostRelief
+     *
+     * @returns {DOMElement} container
+     */
+    _createAdvancedToolElement : function (brOptions) {
+        // exemple :
+        // <div id="GPadvancedToolsRelief_ID_Layer1" class="GPlayerAdvancedToolsRelief">
+        //     <!-- _createAdvancedToolOpacityElement -->
+        // </div>
+
+        var container = document.createElement("div");
+        container.id = this._addUID("GPadvancedToolsRelief_ID_" + brOptions.elevationLayer.id);
+        container.className = "GPlayerAdvancedToolsRelief";
+
+        var array = this._createAdvancedToolReliefElement(brOptions);
+        for (var i = 0; i < array.length; i++) {
+            container.appendChild(array[i]);
+        }
+
+        return container;
+    },
+    /**
+     * Creation de l'icone de gestion du relief du layer (DOM)
+     *
+     * @param {Object} brOptions - Options du control boostRelief
+     *
+     * @returns {DOMElement[]} array of two containers
+     */
+    _createAdvancedToolReliefElement : function (brOptions) {
+        // exemple :
+        // <div id="GPrelief_ID_Layer1" class="GPlayerRelief" title="Opacité">
+        //   <input id="GPreliefRange_ID_Layer1" type="range" value="100" oninput="GPchangeLayerRelief(this);" onchange="GPchangeLayerRelief(this);">
+        // </div>
+        // <div class="GPlayerReliefValue" id="GPreliefValueDiv_ID_Layer1">
+        //   <span id="GPreliefValue_ID_Layer1">100</span>
+        //   %
+        // </div>
+
+        var list = [];
+
+        // curseur pour changer l'opacité
+        var divO = document.createElement("div");
+        divO.id = this._addUID("GPRelief_ID_" + brOptions.elevationLayer.id);
+        divO.className = "GPlayerRelief";
+        divO.title = "Relief";
+
+        // on recupere la valeur actuelle d'echelle du relief depuis la couche donnée en parametre
+        var relief = brOptions.elevationLayer.scale || 1;
+
+        var input = document.createElement("input");
+        input.id = this._addUID("GPreliefValueDiv_ID_" + brOptions.elevationLayer.id);
+        input.type = "range";
+
+        // echelle de 1 à 50 par defaut
+        if (!brOptions.scale) {
+            brOptions.scale = {
+                min : 1,
+                max : 50,
+                step : 1
+            };
+        };
+
+        input.min = brOptions.scale.min || 1;
+        input.max = brOptions.scale.max || 50;
+        input.step = brOptions.scale.step || 1;
+        input.value = relief;
+
+        // add event for relief change
+        var context = this;
+        if (input.addEventListener) {
+            input.addEventListener(
+                "change",
+                function (e) {
+                    context._onChangeLayerRelief(e);
+                }
+            );
+        } else if (input.attachEvent) {
+            // internet explorer
+            input.attachEvent(
+                "onchange",
+                function (e) {
+                    context._onChangeLayerRelief(e);
+                }
+            );
+        }
+
+        if (input.addEventListener) {
+            input.addEventListener(
+                "input",
+                function (e) {
+                    context._onChangeLayerRelief(e);
+                }
+            );
+        } else if (input.attachEvent) {
+            // internet explorer
+            input.attachEvent(
+                "oninput",
+                function (e) {
+                    context._onChangeLayerRelief(e);
+                }
+            );
+        }
+
+        divO.appendChild(input);
+
+        // Valeur d'echelle du relief
+        var divC = document.createElement("div");
+        divC.id = this._addUID("GPreliefValueDiv_ID_" + brOptions.elevationLayer.id);
+        divC.className = "GPlayerReliefValue";
+
+        var span = document.createElement("span");
+        span.id = this._addUID("GPreliefValue_ID_" + brOptions.elevationLayer.id);
+        span.innerHTML = relief + "%";
+
+        divC.appendChild(span);
+
+        list.push(divO);
+        list.push(divC);
+
+        return list;
+    }
+};
+
+export default BoostReliefDOM;
---
+++ src/Itowns/Controls/BoostRelief.js
@@ -0,0 +1,243 @@
+import GlobeViewExtended from "../GlobeViewExtended";
+import Logger from "../../Common/Utils/LoggerByDefault";
+import Utils from "../../Common/Utils";
+import SelectorID from "../../Common/Utils/SelectorID";
+import BoostReliefDOM from "../../Common/Controls/BoostReliefDOM";
+import Widget from "./Widget";
+
+var logger = Logger.getLogger("BoostRelief");
+
+/**
+ * @classdesc
+ * Control to manage globe layers : their order, visibility and relief, and display their informations (title, description, legends, metadata...)
+ *
+ * @constructor
+ * @extends {itowns.control.Widget}
+ * @alias itowns.control.BoostRelief
+ * @param {Object} brOptions - control options
+ * @param {Boolean} [brOptions.options.collapsed = true] - Specify if widget has to be collapsed (true) or not (false) on globe loading.
+ * @example
+ * var boostRelief = new itowns.control.BoostRelief({
+ *  layers : [
+ *      {
+ *          id : "myLayer",
+ *          config : {
+ *              title : "test layer name 1",
+ *              description : "test layer desc 1",
+ *          }
+ *      }
+ *  ],
+ *  options : {
+ *      collapsed : false
+ *  }
+ * ));
+ */
+function BoostRelief (brOptions) {
+    brOptions = brOptions || {};
+    var options = brOptions.options || {};
+
+    if (!(this instanceof BoostRelief)) {
+        throw new TypeError("ERROR CLASS_CONSTRUCTOR");
+    }
+
+    if (options && typeof options !== "object") {
+        throw new Error("ERROR WRONG_TYPE : options should be an object");
+    }
+
+    this._initialize();
+
+    var container = this._initContainer(brOptions);
+    var targetDiv = document.getElementById(options.target) || null;
+
+    Widget.call(
+        this, {
+            name : "BoostRelief",
+            element : container,
+            target : targetDiv
+        }
+    );
+}
+
+/*
+ * @lends module:BoostRelief
+ */
+BoostRelief.prototype = Object.create(Widget.prototype, {});
+
+// retrieves methods of the common class BoostReliefDOM
+Utils.assign(BoostRelief.prototype, BoostReliefDOM);
+
+/**
+ * Constructor (alias)
+ *
+ * @private
+ */
+BoostRelief.prototype.constructor = BoostRelief;
+
+// ################################################################### //
+// ############## public methods (getters, setters) ################## //
+// ################################################################### //
+
+/**
+ * Bind globe to control
+ *
+ * @param {GlobeViewExtended} globe - the globe
+ */
+BoostRelief.prototype.setGlobe = function (globe) {
+    var layers;
+    if (globe) { // in the case the control is added to the globe
+        var self = this;
+
+        // adding of listeners
+        this._callbacks.onReliefLayerCallBack = function (e) {
+            self._updateLayerRelief(e.target.id, e.new.relief);
+        };
+    } else {
+        this._globe.removeLayerListener(layers[j], GlobeViewExtended.EVENTS.Relief_PROPERTY_CHANGED, this._callbacks.onReliefLayerCallBack);
+        // deletes the layerSwitcher DOM
+        while (this._element.hasChildNodes()) {
+            this._element.removeChild(this._element.lastChild);
+        }
+        this._element.parentNode.removeChild(this._element);
+    }
+
+    // calls original setGlobe method
+    Widget.prototype.setGlobe.call(this, globe);
+};
+
+/**
+ * Collapse or display control main container
+ *
+ * @param {Boolean} collapsed - True to collapse control, False to display it
+ */
+BoostRelief.prototype.setCollapsed = function (collapsed) {
+    if (collapsed === undefined) {
+        logger.error("BoostRelief:setCollapsed - missing collapsed parameter");
+        return;
+    }
+    var isCollapsed = this.getCollapsed();
+    if ((collapsed && isCollapsed) || (!collapsed && !isCollapsed)) {
+        return;
+    }
+};
+
+/**
+ * Returns true if widget is collapsed (minimize), false otherwise
+ * @return {Boolean} is collapsed
+ */
+BoostRelief.prototype.getCollapsed = function () {
+    return !document.getElementById(this._addUID("GPshowLayersList")).checked;
+};
+
+// ################################################################### //
+// ##################### init component ############################## //
+// ################################################################### //
+
+/**
+ * Initialize BoostRelief control (called by constructor)
+ *
+ * @private
+ */
+BoostRelief.prototype._initialize = function () {
+    // id of the control ; used to suffix the CSS id (handles cases with severel controls on the same page)
+    this._uid = SelectorID.generate();
+
+    // {Object} control layers list. Each key is a layer id, and its value is an object of layers options (layer, id, relief, visibility, title, description...)
+    this._layers = {};
+
+    // callbacks
+    this._callbacks = {};
+};
+
+/**
+ * Creates control main container
+ *
+ * @method _initContainer
+ * @param {Object} options - control options
+ * @returns {DOMElement} container - widget container
+ * @private
+ */
+BoostRelief.prototype._initContainer = function (brOptions) {
+    // creation of the main container
+    var container = this._createMainContainerElement();
+
+    // adding in the main container
+    var cursor = this._createAdvancedToolElement(brOptions);
+    container.appendChild(cursor);
+
+    return container;
+};
+
+// ################################################################### //
+// ######################### DOM events ############################## //
+// ################################################################### //
+
+/**
+ * Changes layer relief on layer relief picto click
+ *
+ * @method _onChangeLayerRelief
+ * @param {Object} e - HTML event
+ * @private
+ */
+BoostRelief.prototype._onChangeLayerRelief = function (e) {
+    var globe = this.getGlobe();
+    var layerID = this._resolveLayerId(e.target.id);
+
+    // echelle qui commence à 1
+    var reliefValue = parseInt(e.target.value);
+    var reliefId = document.getElementById(this._addUID("GPreliefValue_ID_" + layerID));
+
+    // et qui finit à 101/2 = 50
+    reliefId.innerHTML = "x" + reliefValue;
+    
+    var elevationLayer = globe.getLayerById(layerID);
+    function updateScale(layer, value) {
+        layer.scale = value;
+        globe.notifyChange(layer);
+    }
+    // echelle sur 51
+    updateScale(elevationLayer, reliefValue);
+};
+
+/**
+ * Updates picto relief value on layer relief change
+ *
+ * @method _updateLayerRelief
+ * @param {String} layerId - layer id
+ * @param {Number} relief - relief value
+ * @private
+ */
+/*
+BoostRelief.prototype._updateLayerRelief = function (layerId, relief) {
+    if (relief > 1) {
+        relief = 1;
+    }
+    if (relief < 0) {
+        relief = 0;
+    }
+
+    var layerReliefInput = document.getElementById(this._addUID("GPreliefValueDiv_ID_" + layerId));
+    if (layerReliefInput) {
+        layerReliefInput.value = Math.round(relief * 100);
+    }
+
+    var layerReliefSpan = document.getElementById(this._addUID("GPreliefValue_ID_" + layerId));
+    if (layerReliefSpan) {
+        layerReliefSpan.innerHTML = Math.round(relief * 100) + "%";
+    }
+};
+*/
+
+/**
+ * Gets layer id from div id
+ *
+ * @method _resolveLayerId
+ * @param {String} divId - HTML div id
+ * @returns {String} layer id
+ * @private
+ */
+BoostRelief.prototype._resolveLayerId = function (divId) {
+    var divName = SelectorID.name(divId); // ex GPvisibilityPicto_ID_26
+    return divName.substring(divName.indexOf("_ID_") + 4); // ex. 26
+};
+
+export default BoostRelief;
