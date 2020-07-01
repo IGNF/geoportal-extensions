06cb37e3bff163f3e917cbf9a4a6746cebbcbe97
lgrd <loic.grondin.pro@gmail.com>
2020-07-01 15:57:24 +0200

    feat(docker): utilisation de l'access-lib local lors du build

--- docker/scripts/start.sh
+++ docker/scripts/start.sh
@@ -4,30 +4,48 @@
 if [ ! -d /home/docker/html/geoportal-extensions/dist ]
 then
     mkdir -p /home/docker/html/geoportal-extensions/dist
+else 
+    rm -rf /home/docker/html/geoportal-extensions/dist/*
 fi
 if [ ! -d /home/docker/html/geoportal-extensions/samples ]
 then
     mkdir -p /home/docker/html/geoportal-extensions/samples
+else 
+    rm -rf /home/docker/html/geoportal-extensions/samples/*
 fi
 if [ ! -d /home/docker/html/geoportal-extensions/jsdoc ]
 then
     mkdir -p /home/docker/html/geoportal-extensions/jsdoc
+else 
+    rm -rf /home/docker/html/geoportal-extensions/jsdoc/*
 fi
 if [ ! -d /home/docker/html/geoportal-extensions/package ]
 then
     mkdir -p /home/docker/html/geoportal-extensions/package
+else 
+    rm -rf /home/docker/html/geoportal-extensions/package/*
 fi
 if [ ! -d /home/docker/html/geoportal-extensions/node_modules ]
 then
     mkdir -p /home/docker/html/geoportal-extensions/node_modules
-    pushd /home/docker/geoportal-extensions/
-    cp -r node_modules/* /home/docker/html/geoportal-extensions/node_modules/
-    popd
+else 
+    rm -rf /home/docker/html/geoportal-extensions/node_modules/*
 fi
+
+# on attend que les containers nginx et access-lib soient prêts
+until [ -f /home/docker/html/geoportal-access-lib/package/geoportal-access-lib*.tgz ]; do
+  >&2 echo "Access-lib package is unavailable - sleeping..."
+  sleep 2
+done
+>&2 echo "Access-lib package is available !"
+
+# Modification du package.json pour se baser sur la version d'access-lib qui est en local
+sed -i 's/"\s*geoportal-access-lib\s*"\s*:\s*".*"/"geoportal-access-lib": "file:\/home\/docker\/html\/geoportal-access-lib\/package\/geoportal-access-lib-local.tgz"/g' package.json
 
 # On build au cas où le code ait changé
 pushd /home/docker/geoportal-extensions/
 rm -rf node_modules package-lock.json && npm install
+cp -r node_modules/* /home/docker/html/geoportal-extensions/node_modules/
 npm run build
 cp -rf ./dist/* /home/docker/html/geoportal-extensions/dist/
 cp -rf ./samples/* /home/docker/html/geoportal-extensions/samples/
