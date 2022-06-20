79e222b3641398dd9dfd151328f6da9153ab27cb
lowzonenose <jpbazonnais@gmail.com>
2022-06-20 16:52:52 +0200

    modification webpack : copie des ressources (forcer l'écrasement)

--- build/webpack/webpack.config.leaflet.js
+++ build/webpack/webpack.config.leaflet.js
@@ -314,7 +314,9 @@ module.exports = (env, argv) => {
             new CopyWebpackPlugin([
                 {
                     from : path.join(ROOT, "samples-src", "resources", "**/*"),
-                    to : path.join(ROOT, "samples", "resources")
+                    to : path.join(ROOT, "samples", "resources"),
+                    context : path.join(ROOT, "samples-src", "resources"),
+                    force: true
                 }
             ])
         ]
