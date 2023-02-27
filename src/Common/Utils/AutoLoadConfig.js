53c921686b917866dcb3ab3006f5b61869f9c611
elias couppe <elias.couppe@gmail.com>
2023-02-27 18:27:41 +0100

    adaptations access-lib gpf

--- src/Common/Utils/AutoLoadConfig.js
+++ src/Common/Utils/AutoLoadConfig.js
@@ -1,82 +1,58 @@
-/**
- * @module AutoLoadConfig
- * @alias [private] AutoLoadConfig
- * @description
- * ...
- *
- * @example
- * autoloadconfig();
- */
-var AutoLoadConfig = {
-    /**
-     * CChargement automatique de la clé d'accès à partir du tag data-key
-     *
-     * @function autoloadconfig
-     * @returns {Object} options - undefined ou options
-     */
-    getConfig: function () {
-        var scripts = document.getElementsByTagName("script");
-        for (var i=0; i<scripts.length; i++) {
-            if (!scripts[i].getAttribute("data-key") && !scripts[i].getAttribute("data-timeout")) {
-                continue;
-            } else {
-                var timeout = scripts[i].getAttribute("data-timeout");
-                var key = scripts[i].getAttribute("data-key");
-                // in case of several keys
-                if (key) {
-                    var splitKeys = key.split(/;|,|\|/);
-                    if (splitKeys.length > 1) {
-                        var keys = [];
-                        for (var j = 0; j < splitKeys.length; j++) {
-                            keys.push(splitKeys[j]);
-                        }
-                        key = keys;
-                    }
-                }
-                break;
+import Gp from "geoportal-access-lib";
+
+(function () {
+    var scripts = document.getElementsByTagName("script");
+
+    var key = scripts[scripts.length - 1].getAttribute("data-key");
+    // in case of several keys
+    /*if (key) {
+        var splitKeys = key.split(/;|,|\|/);
+        if (splitKeys.length > 1) {
+            var keys = [];
+            for (var i = 0; i < splitKeys.length; i++) {
+                keys.push(splitKeys[i]);
             }
+            key = keys;
         }
+    }*/
+    var url = scripts[scripts.length - 1].getAttribute("data-url");
+    var timeout = scripts[scripts.length - 1].getAttribute("data-timeout");
 
-        // var url = scripts[scripts.length - 1].getAttribute("data-url");
+    // callback
+    var success = function () {
+        // Pas de messages en mode prod
+        // console.log("GetConfig success!");
+    };
 
-        // callback
-        var success = function () {
-            // Pas de messages en mode prod
-            // console.log("GetConfig success!");
-        };
+    // callback
+    var error = function (e) {
+        throw new Error("Configuration load failed : " + e.message);
+    };
 
-        // callback
-        var error = function (e) {
-            throw new Error("Configuration load failed : " + e.message);
-        };
-
-        if (!key) {
-            // pas de message d'information !
-            console.log("WARNING : parameter 'data-key' missing, then the key has to be filed in the control configuration");
-            return;
-        }
-
-        var options = {
-            apiKey: key,
-            onSuccess: success,
-            onFailure: error
-        };
-
-        // if (url) {
-        //     options.serverUrl = url;
-        //     options.callbackSuffix = "";
-        // }
-
-        if (timeout) {
-            options.timeOut = timeout;
-        }
-
-        // test d'existance de la varibale globale Gp.Config
-        if (!Gp.Config) {
-            // appel du service
-            Gp.Services.getConfig(options);
-        }
+    if (!key && !url) {
+        // pas de message d'information !
+        // console.log("WARNING : parameters missing 'data-key' and 'data-url', the loading of configuration can not be done !");
+        return;
     }
-};
 
-export default AutoLoadConfig;
+    var options = {
+        apiKey : key,
+        onSuccess : success,
+        onFailure : error
+    };
+
+    if (url) {
+        options.serverUrl = url;
+        options.callbackSuffix = "";
+    }
+
+    if (timeout) {
+        options.timeOut = timeout;
+    }
+
+    // test d'existance de la varibale globale Gp.Config
+    if (!Gp.Config) {
+        // appel du service
+        Gp.Services.getConfig(options);
+    }
+})();
--- src/OpenLayers/index.js
+++ src/OpenLayers/index.js
@@ -107,6 +107,7 @@ import Search from "./Controls/Editor/Search";
 import Editor from "./Controls/Editor";
 
 // Les autoload...
+import "../Common/Utils/AutoLoadConfig";
 import "./CRS/AutoLoadCRS";
 
 // export des services
@@ -166,7 +167,7 @@ export { default as ColorUtils } from "../Common/Utils/ColorUtils";
 export { default as MathUtils } from "../Common/Utils/MathUtils";
 /** cf. Gp.Logger */
 export { default as Logger } from "../Common/Utils/LoggerByDefault";
-export { default as AutoLoadConfig } from "../Common/Utils/AutoLoadConfig";
+// export { default as AutoLoadConfig } from "../Common/Utils/AutoLoadConfig";
 
 
 function deepCopy (source, target) {
