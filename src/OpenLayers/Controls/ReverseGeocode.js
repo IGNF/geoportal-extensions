23bf31eaaf4858d655aaf8b7c24e7780aadbe4fb
elias couppe <elias.couppe@gmail.com>
2023-09-13 11:07:25 +0200

    feat(ol:Controls) : adaptation pour pouvoir utiliser le oldService

--- src/OpenLayers/Controls/ReverseGeocode.js
+++ src/OpenLayers/Controls/ReverseGeocode.js
@@ -1023,6 +1023,7 @@ var ReverseGeocode = (function (Control) {
             apiKey : reverseGeocodeOptions.apiKey || this.options.apiKey,
             ssl : this.options.ssl,
             position : this._requestPosition,
+            oldReverseService : reverseGeocodeOptions.oldReverseService || false,
             filterOptions : {
                 type : [this._currentGeocodingType]
             },
